0667898998e47fc8dbf9723b6d8ca234f1b3db1a
Gaurav <royalgk2@gmail.com>
2026-01-03 01:10:31 +0530

    Initial setup: Routes folder setup

--- services/shopping-cart-service/app.js
+++ services/shopping-cart-service/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
-const app = require('express');
+const express = require('express');
+const router = require('./routes/route');
+const app = express();
 
-app.use(express.json());;
+app.use(express.json());
+app.use('/', router);
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
     console.log('Shopping Cart Service is running on port ' + PORT);
