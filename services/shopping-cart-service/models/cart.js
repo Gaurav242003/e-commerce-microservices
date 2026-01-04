86bae7d15ccc28f0dfc99b5d9d17720982a2522d
Gaurav <royalgk2@gmail.com>
2026-01-04 13:22:55 +0530

    Database: Cart schema defined

--- services/shopping-cart-service/models/cart.js
+++ services/shopping-cart-service/models/cart.js
@@ -25,3 +25,23 @@ const cartItemSchema = new Schema(
   { _id: false } // no separate _id for each item; optional
 );
 
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true // one cart per user
+    },
+    items: {
+      type: [cartItemSchema],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+const cartItem = mongoose.model("CartItem", cartItemSchema);
+const cart = mongoose.model("Cart", cartSchema);
+
+module.exports = {cart, cartItem};
