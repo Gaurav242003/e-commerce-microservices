27f2704bf7bd4288091c4a771a1f7a6c5090abd1
Gaurav <royalgk2@gmail.com>
2026-01-04 13:11:19 +0530

    Database: Cart Item schema defined

---
+++ services/shopping-cart-service/models/cart.js
@@ -0,0 +1,27 @@
+const mongoose = require('../database/connectionDB');
+const { Schema } = mongoose;
+
+const cartItemSchema = new Schema(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
+    },
+    name: { type: String, required: true },
+    priceSnapshot: { type: Number, required: true }, // price at time of add
+    currency: { type: String, default: "INR" },
+    imageUrl: String, // optional, for UI display
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    addedAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { _id: false } // no separate _id for each item; optional
+);
+
