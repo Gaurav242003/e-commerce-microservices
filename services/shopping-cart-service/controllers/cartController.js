18ecedb5f0e2033d2f673ed1da9a1b8594560cfe
Gaurav <royalgk2@gmail.com>
2026-01-05 10:50:14 +0530

    Feature: add to cart api implemented

--- services/shopping-cart-service/controllers/cartController.js
+++ services/shopping-cart-service/controllers/cartController.js
@@ -35,4 +35,65 @@ const getCart = async (req, res) => {
 };
 
 
-module.exports = {getCart};
+const addItemToCart = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { productId, quantity } = req.body;
+
+    if (!productId || !quantity || quantity < 1) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid productId or quantity"
+      });
+    }
+
+    const product = await getProductById(productId);
+
+    if (product.status !== "ACTIVE") {
+      return res.status(400).json({
+        success: false,
+        message: "Product is not available"
+      });
+    }
+
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = await Cart.create({ userId, items: [] });
+    }
+
+    const itemIndex = cart.items.findIndex(
+      item => item.productId.toString() === productId
+    );
+
+    if (itemIndex > -1) {
+      cart.items[itemIndex].quantity += quantity;
+    } else {
+      cart.items.push({
+        productId,
+        name: product.name,
+        priceSnapshot: product.price,
+        currency: product.currency,
+        imageUrl: product.imageUrl,
+        quantity
+      });
+    }
+
+    await cart.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Item added to cart",
+      data: cart
+    });
+  } catch (error) {
+    console.error("Add Item Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+};
+
+
+
+module.exports = {getCart, addItemToCart};
