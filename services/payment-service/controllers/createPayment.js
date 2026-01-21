d5a4001bb203532e44cdb7d853a118e1e25a2ebb
Gaurav <royalgk2@gmail.com>
2026-01-21 23:04:12 +0530

    Developemt: create payment business logic implemented

---
+++ services/payment-service/controllers/createPayment.js
@@ -0,0 +1,70 @@
+const {v4: uuidv4} = require('uuid');
+const paymentRepo = require('../repo/payment.repo');
+const { PAYMENT_STATUS } = require('../models/payment.constants');
+const { getIdempotencyKey, acquireLock, setIdempotencyKey, releaseLock } = require('../database/connectionRedis');
+
+
+const createPayment = async (data) => {
+  const {
+    orderId,
+    userId,
+    amount,
+    currency,
+    paymentMethod,
+    idempotencyKey
+  } = data;
+
+  // 1. Check idempotency
+  const cachedPaymentId = await getIdempotencyKey(
+    `payment:idempotency:${idempotencyKey}`
+  );
+
+  if (cachedPaymentId) {
+    return paymentRepo.findById(cachedPaymentId);
+  }
+
+  // 2. Acquire order lock
+  const lockKey = `payment:lock:${orderId}`;
+  const lock = await acquireLock(lockKey);
+
+  if (!lock) {
+    throw new Error("Payment already in progress for this order");
+  }
+
+  try {
+    // 3. Double-check DB idempotency
+    const existingPayment =
+      await paymentRepo.findByIdempotencyKey(idempotencyKey);
+
+    if (existingPayment) {
+      return existingPayment;
+    }
+
+    // 4. Create payment
+    const payment = await paymentRepo.createPayment({
+      id: uuidv4(),
+      orderId,
+      userId,
+      amount,
+      currency,
+      paymentMethod,
+      status: PAYMENT_STATUS.INITIATED,
+      idempotencyKey
+    });
+
+    // 5. Cache idempotency key
+    await setIdempotencyKey(
+      `payment:idempotency:${idempotencyKey}`,
+      payment.id
+    );
+
+    return payment;
+  } finally {
+    // 6. Release lock
+    await releaseLock(lockKey);
+  }
+};
+
+module.exports = {
+  createPayment
+};
--- services/payment-service/database/connectionRedis.js
+++ services/payment-service/database/connectionRedis.js
@@ -24,7 +24,30 @@ const connectRedis = async () => {
   }
 };
 
+const setIdempotencyKey = async (key, value) => {
+  await redisClient.set(key, value, { EX: 600 }); // 10 minutes
+};
+
+const getIdempotencyKey = async (key) => {
+  return redisClient.get(key);
+};
+
+const acquireLock = async (key) => {
+  return redisClient.set(key, "LOCKED", {
+    NX: true,
+    EX: 10
+  });
+};
+
+const releaseLock = async (key) => {
+  await redisClient.del(key);
+};
+
 module.exports = {
   redisClient,
-  connectRedis
+  connectRedis,
+  setIdempotencyKey,
+  getIdempotencyKey,
+  acquireLock,
+  releaseLock
 };
