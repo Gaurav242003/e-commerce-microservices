abefa18563ee5234fa25fa86bc82952f2ae5258e
Gaurav <royalgk2@gmail.com>
2026-02-03 19:18:31 +0530

    new APIs are defined (orderSuccess, orderFailed, orderCompleted, orderCancelled)

--- services/shopping-cart-service/routes/route.js
+++ services/shopping-cart-service/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('../database/connectionDB');
 const { authVerify } = require('../middlewares/auth');
-const { getCart, addItemToCart, updateItemQuantity, removeItemFromCart, validateCart } = require('../controllers/cartController');
+const { getCart, addItemToCart, updateItemQuantity, removeItemFromCart, validateCart, clearCartByUserId } = require('../controllers/cartController');
 const router = express.Router();
 
 router.get('/cart', authVerify, getCart);
@@ -9,6 +9,7 @@ router.post('/cart/add', authVerify, addItemToCart);
 router.put('/cart/update/:productId', authVerify, updateItemQuantity);
 router.delete('/cart/remove/:productId', authVerify, removeItemFromCart);
 router.get('/cart/validate', authVerify, validateCart);
+router.delete('/cart/clear', authVerify, clearCartByUserId);
 
 router.get('/', (req, res) => {
     res.send('Shopping Cart Service is up and running');
